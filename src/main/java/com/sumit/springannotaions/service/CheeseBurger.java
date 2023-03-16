c9a3360a807c20253f7fd3df562b55efa1ee5a2c
sumit Rana <sumit2628r@gmail.com>
2023-03-16 22:40:06 +0530

    primary annotation added

--- src/main/java/com/sumit/springannotaions/service/CheeseBurger.java
+++ src/main/java/com/sumit/springannotaions/service/CheeseBurger.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class CheeseBurger implements Burger {
 
     @Override
